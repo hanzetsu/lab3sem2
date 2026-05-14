4fef10c1c0c3031dc28eb7eeec6ce40e22564969
Edward <eduardsivtsev@mail.ru>
2026-05-14 13:58:13 +0300

    started to work with matrix.hpp

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,3 +1,12 @@
 #include "Sequence.hpp"
 
-class Matrix : Sequence
+template <typename T, template <typename> class Container> //добавить концепты
+class Matrix {
+    private:
+    Container<Container<T>> data;
+    size_t rows;
+    size_t columns;
+    public:
+    Matrix() = delete;
+    Matrix(Container<Container<T>> Data) : data (Data), rows(Data.GetLength()), columns(rows > 0 ? data.Get(0).GetSize() : 0){}
+};
