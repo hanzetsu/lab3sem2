987bae96f7da707e6d13a627355a3b3ef73604b3
Edward <eduardsivtsev@mail.ru>
2026-05-17 19:12:04 +0300

    added file for constants

---
+++ include/Constants.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Constants {
+    inline constexpr double PI = 3.14159265358979323846;
+    inline constexpr double PI_HALF = PI / 2.0;
+    inline constexpr double PI_QUARTER = PI / 4.0;
+    inline constexpr double G = 9.80665;
+}
