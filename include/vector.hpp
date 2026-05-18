0b145f059df7583745651efbeabb4b86997908a7
Edward <eduardsivtsev@mail.ru>
2026-05-18 12:33:37 +0300

    edited vector constructor list

--- include/vector.hpp
+++ include/vector.hpp
@@ -5,39 +5,49 @@
 #include <initializer_list>
 
 template <typename T>
-class Vector {
+class Vector
+{
 private:
     MutableArraySequence<T> comps;
+
 public:
     Vector() = default;
-    explicit Vector(std::size_t dim) : comps(dim) {
+    explicit Vector(std::size_t dim) : comps(dim)
+    {
         for (std::size_t i = 0; i < dim; ++i)
             comps.Set(i, T(0));
     }
-    Vector(std::initializer_list<T> list) : comps(list.size()) {
-        std::size_t i = 0;
-        for (const T& val : list)
-            comps.Set(i++, val);
+    Vector(std::initializer_list<T> list) : comps(list.size())
+    {
+        for (std::size_t i = 0; i < list.size(); ++i)
+        {
+            const T &val = list.begin()[i];
+            comps.Set(i, val);
+        }
     }
     std::size_t size() const { return comps.GetLength(); }
     T operator[](std::size_t i) const { return comps.Get(i); }
     void set(std::size_t i, T val) { comps.Set(i, val); }
     T get(std::size_t i) const { return comps.Get(i); }
-    double length() const {
+    double length() const
+    {
         double sum = 0.0;
         for (std::size_t i = 0; i < size(); ++i)
             sum += static_cast<double>(comps.Get(i) * comps.Get(i));
         return std::sqrt(sum);
     }
-    Vector normalized() const {
+    Vector normalized() const
+    {
         double len = length();
-        if (len < 1e-12) return *this;
+        if (len < 1e-12)
+            return *this;
         Vector result(size());
         for (std::size_t i = 0; i < size(); ++i)
             result.set(i, static_cast<T>(comps.Get(i) / len));
         return result;
     }
-    Vector operator+(const Vector& other) const {
+    Vector operator+(const Vector &other) const
+    {
         if (size() != other.size())
             throw InvalidArgument("Размерности векторов не совпадают");
         Vector result(size());
@@ -45,7 +55,8 @@ public:
             result.set(i, get(i) + other.get(i));
         return result;
     }
-    Vector operator-(const Vector& other) const {
+    Vector operator-(const Vector &other) const
+    {
         if (size() != other.size())
             throw InvalidArgument("Размерности векторов не совпадают");
         Vector result(size());
@@ -53,13 +64,15 @@ public:
             result.set(i, get(i) - other.get(i));
         return result;
     }
-    Vector operator*(T scalar) const {
+    Vector operator*(T scalar) const
+    {
         Vector result(size());
         for (std::size_t i = 0; i < size(); ++i)
             result.set(i, get(i) * scalar);
         return result;
     }
-    T dot(const Vector& other) const {
+    T dot(const Vector &other) const
+    {
         if (size() != other.size())
             throw InvalidArgument("Размерности векторов не совпадают");
         T sum = T(0);
@@ -67,11 +80,14 @@ public:
             sum += get(i) * other.get(i);
         return sum;
     }
-    bool operator==(const Vector& other) const {
-        if (size() != other.size()) return false;
+    bool operator==(const Vector &other) const
+    {
+        if (size() != other.size())
+            return false;
         for (std::size_t i = 0; i < size(); ++i)
-            if (get(i) != other.get(i)) return false;
+            if (get(i) != other.get(i))
+                return false;
         return true;
     }
-    bool operator!=(const Vector& other) const { return !(*this == other); }
+    bool operator!=(const Vector &other) const { return !(*this == other); }
 };
