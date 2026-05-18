8a62b78a3b65a249b2c05324c209825aaff65b51
Edward <eduardsivtsev@mail.ru>
2026-05-18 13:16:07 +0300

    vector using template container

--- include/TrajectoryCalculator.hpp
+++ include/TrajectoryCalculator.hpp
@@ -6,57 +6,45 @@
 #include "Constants.hpp"
 #include <cmath>
 
-class ProjectileMotionCalculator
-{
+class ProjectileMotionCalculator {
 private:
-    double calculateRange(const Vector<double> &vel) const
-    {
+    double calculateRange(const DefaultVector<double>& vel) const {
         if (vel.size() != 2)
             throw InvalidArgument("Вектор скорости должен быть двумерным");
         double time = 2.0 * vel.get(1) / Constants::G;
         return vel.get(0) * time;
     }
 
-    double rangeForAngle(double v0, double angleRad) const
-    {
+    double rangeForAngle(double v0, double angleRad) const {
         return calculateRange(v0, angleRad);
     }
 
 public:
-    double calculateRange(double v0, double angleRad) const
-    {
+    double calculateRange(double v0, double angleRad) const {
         double vx = v0 * std::cos(angleRad);
         double vy = v0 * std::sin(angleRad);
-        Vector<double> vel(2);
+        DefaultVector<double> vel(2);
         vel.set(0, vx);
         vel.set(1, vy);
         return calculateRange(vel);
     }
 
-    bool findAngle(double v0, double targetMin, double targetMax, double eps, double &resultAngle) const
-    {
+    bool findAngle(double v0, double targetMin, double targetMax, double eps, double& resultAngle) const {
         double a = 0.0, b = Constants::PI_QUARTER;
         double fa = rangeForAngle(v0, a);
         double fb = rangeForAngle(v0, b);
-        if (fa > targetMax && fb > targetMax)
-            return false;
-        if (fa < targetMin && fb < targetMin)
-            return false;
-        while ((b - a) > eps)
-        {
+        if (fa > targetMax && fb > targetMax) return false;
+        if (fa < targetMin && fb < targetMin) return false;
+        while ((b - a) > eps) {
             double c = (a + b) / 2.0;
             double fc = rangeForAngle(v0, c);
-            if (fc >= targetMin && fc <= targetMax)
-            {
+            if (fc >= targetMin && fc <= targetMax) {
                 resultAngle = c;
                 return true;
             }
-            if (fc < targetMin)
-                a = c;
-            else if (fc > targetMax)
-                b = c;
-            else
-            {
+            if (fc < targetMin) a = c;
+            else if (fc > targetMax) b = c;
+            else {
                 resultAngle = c;
                 return true;
             }
@@ -66,16 +54,13 @@ public:
         return (finalRange >= targetMin && finalRange <= targetMax);
     }
 
-    bool findV0AndAngle(const MutableArraySequence<double> &v0List,
+    bool findV0AndAngle(const MutableArraySequence<double>& v0List,
                         double targetMin, double targetMax, double eps,
-                        double &bestV0, double &bestAngle, double &bestRange) const
-    {
-        for (std::size_t i = 0; i < v0List.GetLength(); ++i)
-        {
+                        double& bestV0, double& bestAngle, double& bestRange) const {
+        for (std::size_t i = 0; i < v0List.GetLength(); ++i) {
             double v0 = v0List.Get(i);
             double angle;
-            if (findAngle(v0, targetMin, targetMax, eps, angle))
-            {
+            if (findAngle(v0, targetMin, targetMax, eps, angle)) {
                 bestV0 = v0;
                 bestAngle = angle;
                 bestRange = calculateRange(v0, angle);
@@ -85,9 +70,8 @@ public:
         return false;
     }
 
-    Vector<double> getVelocity(double v0, double angleRad) const
-    {
-        Vector<double> vel(2);
+    DefaultVector<double> getVelocity(double v0, double angleRad) const {
+        DefaultVector<double> vel(2);
         vel.set(0, v0 * std::cos(angleRad));
         vel.set(1, v0 * std::sin(angleRad));
         return vel;
--- include/concepts.hpp
+++ include/concepts.hpp
@@ -1,15 +1,12 @@
+#pragma once
 #include <concepts>
+#include <cstddef>
 
 template <typename C>
-concept SequenceLike =
-    requires(C c, std::size_t i, typename C::value_type val) {
-        { c.GetLength() } -> std::convertible_to<std::size_t>;
-
-        { c.Get(i) } -> std::convertible_to<const typename C::value_type &>;
-
-        { c.Set(i, val) };
-
-        { c.GetSubsequence(i, i) };
-
-        { c.Append(val) };
-    };
+concept SequenceLike = requires(C c, std::size_t i, typename C::value_type val) {
+    { c.GetLength() } -> std::convertible_to<std::size_t>;
+    { c.Get(i) } -> std::convertible_to<const typename C::value_type &>;
+    { c.Set(i, val) };
+    { c.GetSubsequence(i, i) };
+    { c.Append(val) };
+};
